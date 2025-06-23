9bb7d803f06987c0044f5f2cb5aa79ea7812b6db
Lutz Wellensiek <Lutz@wellensieks.de>
2025-06-23 21:28:20 +0200

    baudrate

--- uart_test.py
+++ uart_test.py
@@ -2,11 +2,12 @@ import serial
 import time
 
 # Öffne serielle Verbindung auf UART3 (GPIO4/5)
-ser = serial.Serial('/dev/ttyAMA3', 115200, timeout=1)
+ser = serial.Serial('/dev/ttyAMA3', 9600, timeout=1)
 
 # Dauerschleife
 while True:
-    message = '\nHallo vom Raspberry Pi!'
+    message = 'Hallo vom Raspberry Pi!\n'
     print("Sende:", message.strip())
     ser.write(message.encode('utf-8'))
+    ser.flush()
     time.sleep(5)
